12a59953bcc096eab662051c42a970f253712170
Estif017 <estifoteklearegay@gmail.com>
2020-09-30 13:42:30 -0700

    template

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+const App = () => {
+	return (
+		<div>
+			<h1>How fast do you type</h1>
+			<textarea name='text' id='' cols='30' rows='10' />
+			<h3>Time remaining ???</h3>
+			<button>Start</button>
+			<h2>Word count ???</h2>
+		</div>
+	);
+};
 export default App;
